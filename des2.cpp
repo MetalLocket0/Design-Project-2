64f962e1416be5406f2d67887e307dd695ea7200
MetalLocket0 <lealk1942@gmail.com>
2024-03-08 14:45:57 -0500

    up

--- des2.cpp
+++ des2.cpp
@@ -10,7 +10,6 @@ const double XMIN = -2.0;
 const double XMAX =  2.0;
 const double TOL  =  0.00001;
 
-// pretty-print polynomial
 void print_polynomial(vector<double> c) {
     double ci;
     bool flag = true;
@@ -45,6 +44,15 @@ void print_polynomial(vector<double> c) {
     cout << endl;
 }
 
+
+double eval(double x, vector<double> c) {
+ double result = 0.0;
+    for (unsigned long int i = 0; i < c.size(); i++) {
+        result += c.at(i) * pow(x, i);
+    }
+    return result;
+}
+
 // exact (theoretical) area
 double exact(vector<double> c) {
     vector<double> c_int;  // coefficients of integral
@@ -56,27 +64,64 @@ double exact(vector<double> c) {
     return eval(XMAX, c_int) - eval(XMIN, c_int); // fundamental theorem of calculus
 }
 
+
+
+
 // read coefficients from cin, until empty newline
 vector<double> read_coefficients(void) {
-    string userInput; 
-    getline ( cin , userInput);
-
-    double userCoe = stod (userInput);
-
-    return userCoe;
+   vector<double> coefficients;
+    string input;
+    
+    while (true) {
+        getline(cin, input);
+        if (input.empty()) {
+            break; 
+        }
+        coefficients.push_back(stod(input));
+    }
+    return coefficients ;
 }
 
-// evaluate polynomial given by coefficients c at x
-double eval(double x, vector<double> c) {
-    // your code here
-}
 
 double estimate(double step, vector<double> c) {
-    // your code here
+  double result = 0.0;
+    for (double x = XMIN; x < XMAX; x += step) {
+        result += eval(x, c) * step;
+    }
+    return result;
 }
-
 int main(void) {
 
 
+    vector<double> coefficients = read_coefficients();
+    
+    double newApproximation = 0.0;
+    double prevApproximation;
+    
+    double rectangle = 10.0;
+
+    double step = (XMAX - XMIN) / rectangle;
+    
+    double val = 1.0;
+    
+
+    int iterations = 0;
+    while (val > TOL) {
+        prevApproximation = newApproximation;
+        rectangle *= 2;
+        step = (XMAX - XMIN) / rectangle;
+        newApproximation = estimate(step, coefficients);
+        iterations++;
+        val = abs(prevApproximation - newApproximation);
+    }
+
+    cout << "Solution:"<< endl;
+    print_polynomial(coefficients);
+    cout << endl; 
+    cout << "Estimate = " << fixed << setprecision(8) << newApproximation << endl;
+    cout << "Exact = " << fixed << setprecision(8) << exact(coefficients) << endl;
+    cout << "Error = " << fixed << setprecision(8) << abs(prevApproximation - exact(coefficients)) << endl;
+    cout << "Iterations = " << iterations << endl;
+    cout << "Step = " << step << endl;
     return 0;
 }
